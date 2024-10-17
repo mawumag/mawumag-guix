5225836d3c5ee16732b945e3e9c93e2a7a6fb972
Marco Baggio <70693636+mawumag@users.noreply.github.com>
2024-10-17 12:47:02 +0200

    add: tresorit

---
+++ mawumag/utils.scm
@@ -0,0 +1,24 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;; Copyright © 2019 Pierre Neidhardt <mail@ambrevar.xyz>
+;;; Copyright © 2020 Jonathan Brielmaier <jonathan.brielmaier@web.de>
+
+(define-module (mawumag utils)
+  #:use-module (srfi srfi-26)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 textual-ports)
+  #:use-module (ice-9 popen)
+  #:use-module (guix utils)
+  #:use-module (guix packages))
+
+(define-public (to32 package64)
+  "Build package for i686-linux.
+Only x86_64-linux and i686-linux are supported.
+- If i686-linux, return the package unchanged.
+- If x86_64-linux, return the 32-bit version of the package."
+  (match (%current-system)
+    ("x86_64-linux"
+     (package
+       (inherit package64)
+       (arguments `(#:system "i686-linux"
+                    ,@(package-arguments package64)))))
+    (_ package64)))
